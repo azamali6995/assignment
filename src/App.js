cc77381c38a021084378a0426b30848804fdb51e
Azam <azamali6995@gmail.com>
2021-04-02 02:52:56 +0500

    add home container

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Container/Home'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
