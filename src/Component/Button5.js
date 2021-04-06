13ad868aa3217878aca54ed79571e1acaff287ab
Azam <azamali6995@gmail.com>
2021-04-06 05:31:40 +0500

    Added Button

---
+++ src/Component/Button5.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Button5() {
+  return (
+    <div>
+      <div className="try_now_btn">
+        <a href="/order">
+          <button className="btn"> Try Today </button>
+        </a>
+      </div>
+    </div>
+  )
+}
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Navbar1 from './Component/Navbar1'
 import Video2 from './Component/Video2'
 import Ground3 from './Component/Ground3'
-
+import Button5 from './Component/Button5'
 
 
 export default function Home() {
@@ -13,6 +13,7 @@ export default function Home() {
       <Navbar1 />
       <Video2 />  
       <Ground3 />
+      <Button5 />
     </div>
   )
 }
